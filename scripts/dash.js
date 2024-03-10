41308cdeedb0212820dbe533d0e243d3e2ac4d36
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-03-11 01:25:12 +0200

    added-addTransaction

--- scripts/dash.js
+++ scripts/dash.js
@@ -1,7 +1,8 @@
 const transactionsContainer = document.getElementById('transactions');
 
 
-const fillTransactions=()=>{
+
+const loadTransactions=()=>{
 
     transactionsContainer.innerHTML=''
     transactions = JSON.parse(window.localStorage.getItem('transactions'));
@@ -19,8 +20,41 @@ const fillTransactions=()=>{
         </tr>`
     })
 
+    
+}
+
+function addTransaction(event) {
+    let amount = event.target.amount.value;
+    let currency = event.target.currency.value;
+    let description = event.target.description.value;
+    let type = event.target.type.value;
+    transactions = JSON.parse(window.localStorage.getItem('transactions'));
+    let transactionsCount = JSON.parse(window.localStorage.getItem('transactionsCount'));
+    if (transactionsCount == null) {
+        transactionsCount = 1;
+    }
+    else
+    {
+        transactionsCount += 1;
+    }
+
+    window.localStorage.setItem('transactionsCount', JSON.stringify(transactionsCount));
+    let newTransaction = {
+        id: transactionsCount,
+        amount: amount,
+        currency: currency,
+        description: description,
+        type: type,
+    }
+
+    if (!transactions) {
+        transactions = [];
+    }
+    transactions.push(newTransaction);
+
+    window.localStorage.setItem('transactions', JSON.stringify(transactions));
+    window.location.reload();
 }
 
 
-
-fillTransactions();
+loadTransactions();
